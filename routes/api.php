74a9cc51908e0624f1568953c07aec99133dc7b8
John <john.monsalve94@gmail.com>
2025-01-16 18:53:45 -0500

    se agregagar los end point de usuario y tasks

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::prefix('users')->group(function () {
+    Route::apiResource('',UserController::class)->only('index');
+    Route::get('{user}/tasks',[UserController::class,'userTasks'])->name('user.tasks');
+});
+Route::apiResource('tasks', TaskController::class)->only(['store', 'update', 'destroy']);
+
