2f4dd7f8400c86946843339544b2bdc59b284988
John <john.monsalve94@gmail.com>
2025-01-17 13:28:34 -0500

    Se implemento jwt para la autentificacion

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 2. copiar el .env.example - linux cp .env.example .env
 3. generar app_key - php artisan key:generate
 4. correr migraciones y seeders - php artisan migrate --seed
+5. para  que ek middleware funcione tenemos que generar un token con el comando - php artisan jwt:secret
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -41,8 +43,33 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        
     ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
+
     public function tasks():HasMany
     {
         return $this->hasMany(Task::class);
