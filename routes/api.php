e472f231dc2b5228bcc51adf16b793ae79787bd1
John <john.monsalve94@gmail.com>
2025-01-17 13:48:45 -0500

    Se arreglo la autentificacion en las rutas

--- routes/api.php
+++ routes/api.php
@@ -22,12 +22,12 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::group([
 
-    'middleware' => 'api',
+    'middleware' => 'auth:api',
     'prefix' => 'auth'
 
 ], function ($router) {
 
-    Route::post('login', [AuthController::class,'login']);
+    Route::post('login', [AuthController::class,'login'])->withoutMiddleware('auth:api');
     Route::post('logout', [AuthController::class,'logout']);
     Route::post('refresh', [AuthController::class,'refresh']);
     Route::post('me', [AuthController::class,'me']);
@@ -37,5 +37,7 @@ Route::prefix('users')->group(function () {
     Route::apiResource('',UserController::class)->only('index');
     Route::get('{user}/tasks',[UserController::class,'userTasks'])->name('user.tasks');
 });
-Route::apiResource('tasks', TaskController::class)->only(['store', 'update', 'destroy']);
+Route::apiResource('tasks', TaskController::class)
+    ->only(['store', 'update', 'destroy'])
+    ->middleware('auth:api');
 
